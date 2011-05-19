f09aa9f650cc5e17f02b8c54eeb261078e190b41
Tiago Rodrigo Kepe <trk08@c3sl.ufpr.br>
2011-05-19 17:31:41 -0300

    Arrumar - Roger fará

--- projeto/src/com/ufpr/gdd/Aplicacao.java
+++ projeto/src/com/ufpr/gdd/Aplicacao.java
@@ -1,26 +1,71 @@
 package com.ufpr.gdd;
+
+import org.mpisws.p2p.filetransfer.FileTransfer;
+
 import rice.p2p.commonapi.Application;
 import rice.p2p.commonapi.Endpoint;
 import rice.p2p.commonapi.Id;
+import rice.p2p.commonapi.IdFactory;
 import rice.p2p.commonapi.Message;
+import rice.p2p.commonapi.Node;
 import rice.p2p.commonapi.NodeHandle;
 import rice.p2p.commonapi.RouteMessage;
 import rice.pastry.PastryNode;
 
+/**
+ * 
+ * @author trk08
+ * 
+ */
+public class Aplicacao implements Application {
+	private static Endpoint endpoint;
+	private static Node node;
+	private static FileTransfer fileTransfer;
 
-public class Aplicacao implements Application{
+	public static Endpoint getEndpoint() {
+		return endpoint;
+	}
 
-	protected Endpoint endpoint;
+	public static void setEndpoint(Endpoint endpoint) {
+		Aplicacao.endpoint = endpoint;
+	}
+
+	public static Node getNode() {
+		return node;
+	}
+
+	public static void setNode(Node node) {
+		Aplicacao.node = node;
+	}
+
+	public static FileTransfer getFileTransfer() {
+		return fileTransfer;
+	}
+
+	public static void setFileTransfer(FileTransfer fileTransfer) {
+		Aplicacao.fileTransfer = fileTransfer;
+	}
+
+	public Aplicacao(PastryNode node,  final IdFactory factory) {
+		Aplicacao.endpoint = node.buildEndpoint(this, "myInstance");
+		Aplicacao.node = node;
+		
+		Aplicacao.endpoint.accept(new AppSocketReceiverImpl());
+		Aplicacao.endpoint.register();
+	}
 	
 	public Aplicacao(PastryNode node) {
-		this.endpoint = node.buildEndpoint(this, "myInstance");		
-		this.endpoint.accept(new AplicacaoSocket());
+		Aplicacao.endpoint = node.buildEndpoint(this, "myInstance");
+		Aplicacao.node = node;
 		
-		this.endpoint.register();
+		Aplicacao.endpoint.accept(new AppSocketReceiverImpl());
+		Aplicacao.endpoint.register();
 	}
+	
+
 	@Override
 	public void deliver(Id arg0, Message arg1) {
-		System.out.println(this+"received "+arg1);
+		System.out.println(this + "received " + arg1);
 	}
 
 	@Override
@@ -29,8 +74,6 @@ public class Aplicacao implements Application{
 	}
 
 	@Override
-	public void update(NodeHandle arg0, boolean arg1) {		
+	public void update(NodeHandle arg0, boolean arg1) {
 	}
-	
-
 }
